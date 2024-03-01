8380d8b86913d298bd9ba464a6419516f453392c
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-03-01 05:18:41 -0500

    made projects center in screen

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "./projectCard"
 export default function Projects() {
     return <section id="projects" className="p-10">
         <h2 className="text-4xl py-2">My Projects</h2>
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap gap-4 justify-center">
         {projects.map(p => <ProjectCard {...p} key={p.name} />)}
         </div>
     </section>
