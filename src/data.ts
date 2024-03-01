0e201a494f0fbdc463c72a168987abd61a492f5a
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-03-01 16:42:42 -0500

    added rust music maker projext

--- src/data.ts
+++ src/data.ts
@@ -35,11 +35,17 @@ export const projects = [
         description: "An offline-first todo PWA. This fullstack app was built in sveltekit with handrolled auth and is still in development.",
         imageSrc: "/todo.png",
         url: "https://capable-praline-7301a7.netlify.app/"
-     }
+     },
+     {
+        name: "Rust Music Maker",
+        description: "This app written in rust and designed as an exploration in writing files to match specifications allows users to make songs, save them to a custom file format and export them to wav files.",
+        imageSrc: "/music.png",
+        url: "https://www.github.com/buxogabriel/simple-files"
+     },
 ] as const
 
 export const Titles = [
     "Software Engineer",
     "Computer Scientist",
-    "Machine Learning enthusiast"
+    "Machine Learning Enthusiast"
 ] as const
