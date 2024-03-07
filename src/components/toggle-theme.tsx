03260d3ac7afa11a52d6813beb9f2479a0baf1eb
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-03-06 19:35:04 -0500

    fixed sizing issues on CTA buttons and theme change button

--- src/components/toggle-theme.tsx
+++ src/components/toggle-theme.tsx
@@ -7,7 +7,7 @@ import { IoSunny, IoMoon } from "react-icons/io5";
 export default function ToggleTheme() {
     const { theme, setTheme } = useContext(ThemeContext);
     let lightTheme = theme == 'light';
-    return <button className="fixed right-0 top-0 md:right-10 md:top-10" onClick={() => setTheme && setTheme(t => lightTheme? 'dark': 'light')}>
+    return <button className="fixed right-0 top-0 md:right-5 md:top-5" onClick={() => setTheme && setTheme(t => lightTheme? 'dark': 'light')}>
         { lightTheme? <IoSunny className="w-16 h-16" />: <IoMoon className="w-16 h-16" />}
     </button>
 }
