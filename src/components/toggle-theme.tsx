d8398eb1c83b8355aad70b9d05176f407324a95c
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-03-06 19:37:53 -0500

    added aria label to theme change button

--- src/components/toggle-theme.tsx
+++ src/components/toggle-theme.tsx
@@ -7,7 +7,7 @@ import { IoSunny, IoMoon } from "react-icons/io5";
 export default function ToggleTheme() {
     const { theme, setTheme } = useContext(ThemeContext);
     let lightTheme = theme == 'light';
-    return <button className="fixed right-0 top-0 md:right-5 md:top-5" onClick={() => setTheme && setTheme(t => lightTheme? 'dark': 'light')}>
+    return <button aria-label="Change visual theme button" className="fixed right-0 top-0 md:right-5 md:top-5" onClick={() => setTheme && setTheme(t => lightTheme? 'dark': 'light')}>
         { lightTheme? <IoSunny className="w-16 h-16" />: <IoMoon className="w-16 h-16" />}
     </button>
 }
