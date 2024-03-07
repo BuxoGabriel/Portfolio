8470dbf8e763735f20b24e27c5f84e357ee21ab7
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-03-06 21:31:10 -0500

    changed about me text

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -4,10 +4,10 @@ export default function AboutMe() {
     return <section id="about" className="py-10">
         <h2 className="text-4xl py-2">About Me</h2>
         <p className={`text-left mx-auto w-10/12 md:w-2/3 lg:w-1/2 min-w-64 text-lg ${fontLight.className}`}>
-            Hello! My name is Gabriel Buxo and I have been coding for over a decade.
-            I have been a Javascript programmer since middle school and I have always been interested in learning and aquiring new skills.
-            I always strive to produce quality software and be a valuable and collaborative team member. 
-            My passion for cs has led me to make countless apps and programs some of which are below with more to come!
+            Hello! I&apos;m Gabriel Buxo. With over a decade of coding experience, I began my journey as a Javascript enthusiast in middle school.
+            Throughout the years, I&apos;ve maintained a keen interest in expanding my skill set and honing my craft.
+            I am dedicated to delivering high-quality software solutions and fostering collaboration within teams.
+            My passion for computer science has driven me to develop numerous apps and programs, a selection of which is showcased below, with many more projects in the pipeline.
         </p>
         {/*TODO: Image of Me*/}
     </section>
