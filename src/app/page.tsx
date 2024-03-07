09b00d3bccf3a002617512c88287e32e6028e10c
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-03-06 20:12:34 -0500

    changed default theme to dark

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import { useState } from "react"
 
 
 export default function Home() {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light')
+  const [theme, setTheme] = useState<'light' | 'dark'>('dark')
   return (
     <main 
       id="top" 
