73a52c02c08ea86182d3641d3ad8ae6dfb3700da
Gabriel Buxo <gabrielbuxo1@gmail.com>
2024-03-06 21:01:33 -0500

    added p5 support

---
+++ src/components/p5-page.tsx
@@ -0,0 +1,7 @@
+export default function P5Page(src: string) {
+    <>
+        <canvas id="canvas" />
+        <script defer src="https://cdn.jsdelivr.net/npm/p5@1.9.1/lib/p5.js"></script>
+        <script defer src={src} />
+    </>
+}
